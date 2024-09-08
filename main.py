d431d14ef81f8ef04c5fcac0fe6e9c012ae24e54
Parsa-Mah <m.n.aparsam@gmail.com>
2024-09-08 07:43:03 +0330

    Part one
    Learned how to clean up data, how to access such data and remove corrupted ones, Also how and when to check for duplicate data and remove thoes on what criteria (highest number of reviews)

---
+++ main.py
@@ -0,0 +1,64 @@
+from csv import reader
+
+### The Google Play data set ###
+opened_file = open('googleplaystore.csv')
+read_file = reader(opened_file)
+android = list(read_file)
+android_header = android[0]
+android = android[1:]
+
+### The App Store data set ###
+opened_file = open('AppleStore.csv')
+read_file = reader(opened_file)
+ios = list(read_file)
+ios_header = ios[0]
+ios = ios[1:]
+
+
+def explore_data(dataset, start, end, rows_and_columns=False):
+    dataset_slice = dataset[start:end]
+    for row in dataset_slice:
+        print(row)
+        print('\n')  # adds a new (empty) line between rows
+
+    if rows_and_columns:
+        print('Number of rows:', len(dataset))
+        print('Number of columns:', len(dataset[0]))
+
+
+print(android_header)
+print('\n')
+explore_data(android, 10472, 10473, False)
+
+print(ios_header)
+print('\n')
+explore_data(ios, 0, 3, True)
+
+print(android[10472])  # incorrect row
+print('\n')
+print(android_header)  # header
+print('\n')
+print(android[0])      # correct row
+
+print(len(android))
+del android[10472]  # don't run this more than once
+print(len(android))
+
+for app in android:
+    name = app[0]
+    if name == 'Instagram':
+        print(app)
+
+duplicate_apps = []
+unique_apps = []
+
+for app in android:
+    name = app[0]
+    if name in unique_apps:
+        duplicate_apps.append(name)
+    else:
+        unique_apps.append(name)
+
+print('Number of duplicate apps:', len(duplicate_apps))
+print('\n')
+print('Examples of duplicate apps:', duplicate_apps[:15])
